9a708d66845a0062806dd054376e38ef9bfcfb28
bkhan2 <mohammed.khan@ons.gov.uk>
2021-02-17 10:11:09 +0000

    tests for reader

---
+++ tests/test_reader.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import patch
+
+from app import BUCKET_NAME, PROJECT_ID
+from app.reader import read
+
+
+class TestReader(unittest.TestCase):
+
+    @patch('app.reader.storage')
+    def test_reader(self, mock_storage):
+        filename = 'test_file'
+        storage_client = mock_storage.Client
+        bucket = storage_client(PROJECT_ID).bucket
+        blob = bucket(BUCKET_NAME).blob
+        data_bytes = blob.download_as_bytes
+        read(filename)
+        storage_client.assert_called_with(PROJECT_ID)
+        bucket.assert_called_with(BUCKET_NAME)
+        blob.assert_called_with(f"/{filename}")
+        data_bytes.return_value.decode.return_value = b"file_content"
