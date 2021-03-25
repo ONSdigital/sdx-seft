db82853fa4c4de22dbe339d381f11dd52fc9cd50
TomHolroyd <tomholroyd96@gmail.com>
2021-03-25 10:49:36 +0000

    working exceptions

--- app/quarantine.py
+++ app/quarantine.py
@@ -3,5 +3,7 @@ from app import CONFIG
 
 def quarantine_submission(message, tx_id: str, error: str):
     data = message.data
+    if tx_id is None:
+        tx_id = 'No tx_id provided'
     future = CONFIG.QUARANTINE_SEFT_PUBLISHER.publish(CONFIG.QUARANTINE_TOPIC_PATH, data, tx_id=tx_id, error=error)
     return future.result()
