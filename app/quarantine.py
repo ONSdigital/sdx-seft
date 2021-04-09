c570c19e566d5f825cb2c6c7758fbd98bac31b96
Chaitali15 <chaitu.tanawade@gmail.com>
2021-04-09 15:08:09 +0100

    unit testing

--- app/quarantine.py
+++ app/quarantine.py
@@ -1,7 +1,7 @@
 from app import CONFIG
 
 
-def quarantine_submission(message, tx_id: str, error: str):
+def quarantine_submission(message: object, tx_id: str, error: str):
     data = message.data
     if tx_id is None:
         tx_id = 'No tx_id provided'
--- tests/test_deliver.py
+++ tests/test_deliver.py
@@ -1,13 +1,15 @@
 import unittest
+from requests.exceptions import ConnectionError
 import requests
 from unittest.mock import patch
+from requests import Session
+from urllib3.exceptions import MaxRetryError
 
-from app.deliver import deliver_seft
+from app.deliver import deliver_seft, post
 from app.errors import QuarantinableError, RetryableError
 
 
 class TestDeliver(unittest.TestCase):
-
     r = requests.Response()
     seft_message = {
         'tx_id': '0f534ffc-9442-414c-b39f-a756b4adc6cb',
@@ -20,7 +22,6 @@ class TestDeliver(unittest.TestCase):
         return str(submission_exception.exception)
 
     def test_deliver_seft(self):
-
         with patch('app.deliver.post') as mock_post:
             mock_post.return_value = self.r
             self.r.status_code = 200
@@ -31,7 +32,8 @@ class TestDeliver(unittest.TestCase):
         with patch('app.deliver.post') as mock_post:
             mock_post.return_value = self.r
             self.r.status_code = 400
-            quarantine_exception_str = self.deliver_bad_response(self.seft_message, b'this is some bytes', QuarantinableError)
+            quarantine_exception_str = self.deliver_bad_response(self.seft_message, b'this is some bytes',
+                                                                 QuarantinableError)
             self.assertEqual(quarantine_exception_str, quarantine_response)
 
     def test_retry_error(self):
@@ -41,3 +43,36 @@ class TestDeliver(unittest.TestCase):
             self.r.status_code = 300
             retry_exception_str = self.deliver_bad_response(self.seft_message, b'this is some bytes', RetryableError)
             self.assertEqual(retry_exception_str, retry_response)
+
+    @patch.object(Session, 'post')
+    def test_post(self, mock_response_post):
+        filename = "seft_survey"
+        seft_message = {
+            'tx_id': '0f534ffc-9442-414c-b39f-a756b4adc6cb',
+            'filename': 'seft_survey'
+        }
+        mock_response_post.return_value.status_code = 200
+        post(filename, seft_message)
+        mock_response_post.assert_called()
+
+    @patch('app.deliver.session')
+    def test_post_max_retry_exception(self, mock_session):
+        mock_session.post.side_effect = MaxRetryError("pool", "url", "reason")
+        filename = "seft_survey"
+        seft_message = {
+            'tx_id': '0f534ffc-9442-414c-b39f-a756b4adc6cb',
+            'filename': 'seft_survey'
+        }
+        with self.assertRaises(RetryableError):
+            post(filename, seft_message)
+
+    @patch('app.deliver.session')
+    def test_post_connection_exception(self, mock_session):
+        mock_session.post.side_effect = ConnectionError()
+        filename = "seft_survey"
+        seft_message = {
+            'tx_id': '0f534ffc-9442-414c-b39f-a756b4adc6cb',
+            'filename': 'seft_survey'
+        }
+        with self.assertRaises(RetryableError):
+            post(filename, seft_message)
