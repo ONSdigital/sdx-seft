ffdcb619ba16edc9693ebb658ce298329582544c
ldearsley <ldearsley2@gmail.com>
2023-04-04 10:19:14 +0100

    remove subscriber.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import structlog
 from waitress import serve
 
-from app import subscriber, cloud_config
+from app import app, cloud_config
 
 logger = structlog.get_logger()
 
@@ -9,4 +9,4 @@ logger = structlog.get_logger()
 if __name__ == '__main__':
     logger.info('Starting SDX SEFT')
     cloud_config()
-    serve(subscriber, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
