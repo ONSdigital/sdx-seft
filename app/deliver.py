4003d0c1679070268e2a114f7b8935ce290d3130
Jon Kent <jonkent95@gmail.com>
2023-05-22 15:14:03 +0100

    Post tx_id (#37)

--- app/deliver.py
+++ app/deliver.py
@@ -7,6 +7,7 @@ from app import CONFIG, sdx_app
 METADATA_FILE = 'metadata'
 FILE_NAME = 'filename'
 SEFT_FILE = 'seft'
+TX_ID = "tx_id"
 UTF8 = "utf-8"
 
 logger = get_logger()
@@ -23,7 +24,8 @@ def deliver_seft(meta_dict: dict[str, str], file_bytes: bytes):
         SEFT_FILE: file_bytes}
 
     filename = meta_dict['filename']
+    tx_id = meta_dict['tx_id']
 
     domain = CONFIG.DELIVER_SERVICE_URL
     endpoint = "deliver/seft"
-    sdx_app.http_post(domain, endpoint, None, params={FILE_NAME: filename}, files=files)
+    sdx_app.http_post(domain, endpoint, None, params={FILE_NAME: filename, TX_ID: tx_id}, files=files)
