98c5d9b3c05a7fe2514927b0668dff770a3c2937
Angus <Angus.goody@ons.gov.uk>
2026-04-13 15:29:21 +0100

    tweaks

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
+import base64
+import json
 
 from fastapi import APIRouter, Depends
 from sdx_base.errors.errors import DataError
-from sdx_base.models.pubsub import Message, get_message
+from sdx_base.models.pubsub import Message, get_message, get_data
 from starlette.requests import Request
 from starlette.responses import Response
 
 from app.definitions.definitions import Metadata
 from app.dependencies import get_process_service, get_receipt_service
+from app.functions.filename_function import parse_metadata_from_filename
 from app.services.process_service import ProcessService
 from app.services.receipt_service import ReceiptService
 
@@ -19,22 +22,36 @@ async def handle(
         process_service: ProcessService = Depends(get_process_service),
         receipt_service: ReceiptService = Depends(get_receipt_service),
 ) -> Response:
-    # Fetch the Pub/Sub message
-    message: Message = await get_message(request)
-    meta_dict: Metadata = {}
+
+    # TODO no idea if this will work
+
+    # Fetch the content
+    content = await request.json()
+
+    # Extract data from request
+
+    data: str = base64.b64decode(content).decode("utf-8").strip()
+    data: dict = json.loads(data)
+
+    file_name = data["name"]
+
+    # Work out other metadata from file_name
+    metadata: Metadata | None = parse_metadata_from_filename(file_name)
+
+    if not metadata:
+        raise DataError("Failed to parse metadata from filename!")
 
     try:
-        # Extract metadata from the message
-        meta_dict = process_service.process_metadata(message)
-        # Process the message and deliver the SEF file
-        process_service.process_seft(meta_dict)
+        process_service.process_seft(metadata)
     except DataError as e:
+
+        # TODO fix this
         process_service.quarantine_message(message, str(e))
         # Ack message (even in case of error, as it has been quarantined)
         return Response(status_code=204)
 
     # Process the receipt for SEFT file
-    receipt_service.process_receipt(meta_dict)
+    receipt_service.process_receipt(metadata)
 
     # Ack message
     return Response(status_code=204)
