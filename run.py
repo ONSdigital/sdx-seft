95840eadbaa3479b232907c0931979a69546bc45
jamesbryer <james.bryer@ons.gov.uk>
2026-04-14 14:37:48 +0100

    tweak

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from sdx_base.run import run
 from sdx_base.server.server import RouterConfig
+from sdx_base.server.tx_id import txid_not_applicable
 
 from app.routes import router
 from app.services.process_service import get_tx_id
@@ -9,5 +10,5 @@ from app.settings import Settings
 
 if __name__ == '__main__':
     proj_root = Path(__file__).parent  # sdx-seft dir
-    router_config = RouterConfig(router, tx_id_getter=get_tx_id)
+    router_config = RouterConfig(router, tx_id_getter=txid_not_applicable)
     run(Settings, routers=[router_config], proj_root=proj_root)
